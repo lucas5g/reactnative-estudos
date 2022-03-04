7eedb78f826f7fa5d91082ffcb524de64a7c011c
lucas.sousa <lucas.sousa@codificar.com.br>
2022-03-04 00:33:04 -0300

    fix folder

---
+++ modernuifix/src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+import { Container } from './styles'
+
+
+export function Home(){
+    return (
+        <Container>
+            <Text>Home 123</Text>
+        </Container>        
+    )
+}
